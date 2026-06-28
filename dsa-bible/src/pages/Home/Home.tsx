e72f1eaad786a2ae699ad29f8b8ac90abea7ef2a
Gaurav Singh <109308730+raghuvanshi01@users.noreply.github.com>
2026-06-28 23:01:38 +0530

    commit-001-project-bootstrap

---
+++ dsa-bible/src/pages/Home/Home.tsx
@@ -0,0 +1,37 @@
+import "./Home.css";
+
+function Home() {
+
+return(
+
+<>
+
+<h1>
+
+Welcome to the DSA Bible
+
+</h1>
+
+<p>
+
+Learn Algorithms by Recognizing Patterns.
+
+</p>
+
+<div className="hero">
+
+<button>
+
+Start Learning
+
+</button>
+
+</div>
+
+</>
+
+)
+
+}
+
+export default Home;
